411dd9f7fe0119baf42487c198f2b96cdbbac16a
Aziz ullah <uaziz9164@gmail.com>
2025-06-25 12:07:40 +0200

    navbar

--- src/navbar/nav.jsx
+++ src/navbar/nav.jsx
@@ -24,33 +24,37 @@ const Nav = () => {
         <div className='mt-2'>
              <h1 className='text-2xl font-bold text-white'>Aziz Ullah</h1>
         </div>
-        <div className='md:block hidden'>
-            <ul className='flex lg:gap-5 gap-3 justify-center items-center mt-2 text-white font-bold text-xl'>
-                <li className='hover:text-xl hover:text-pink-500 hover:font-bold transition duration-400 ease-in-out'>Home</li>
-                <li>About</li>
-                <li>Education</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-        <div>
-            <button className=' border py-2 px-4 bg-blue-500 text-white rounded-full md:block hidden'>Contact with me</button>
-        </div>
-
-        <div className='flex gap-4 items-center md:hidden block'>
-          <div className='md:hidden block cursor-pointer' ref={bar} onClick={show}>
+        <div className='md:r-0 r-[-365px]'>
+          
+         <div className='flex gap-4 items-center md:hidden block'>
+           <div className='md:hidden block cursor-pointer' ref={bar} onClick={show}>
           <i class="fa-solid fa-bars-staggered text-white text-2xl "></i>
           </div>
            
            <div className='hidden cursor-pointer' ref={time} onClick={hide}>
            <i class="fa-regular fa-circle-xmark text-white text-2xl"></i>
-           </div>
+           </div> 
+           
+        </div> 
+        <div className='md:block hidden w-full' ref={list}>
+        <ul className='flex md:flex-row flex-col lg:gap-5 gap-3 justify-center items-start mt-2 text-white font-bold text-xl '>
+                <li >Home</li>
+                <li>About</li>
+                <li>Training</li>
+                <li>Skills</li>
+                <li>Services</li>
+                <li>Projects</li>
+            </ul>
+        </div>
            
         </div>
+        <div className='md:block hidden'>
+            <button className=' border py-2 px-4 bg-blue-500 text-white rounded-full'>Contact with me</button>
+        </div>
+
       </div>
 
-      <div className='flex flex-col py-8 px-5 gap-1 bg-black w-55 float-right hidden' ref={list}>
+      {/* <div className='flex flex-col py-8 px-5 gap-1 bg-black w-55 float-right hidden' ref={list}>
 
       <div>
             <ul className='flex flex-col gap-3 justify-center items-center mt-2 text-white font-bold'>
@@ -66,7 +70,7 @@ const Nav = () => {
             <button className=' border py-2 px-4 bg-blue-500 text-white rounded-md'>Contact with me</button>
         </div>
 
-      </div>
+      </div> */}
     </div>
   )
 }
